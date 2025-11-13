ca772e2c4930fc6f6fc1db05fbf0a8ef34e14b61
peter.fux <peter.fux@willhaben.at>
2025-11-13 19:45:48 +0100

    initialize project

--- test/date_time_test.dart
+++ test/date_time_test.dart
@@ -1,4 +1,4 @@
-import 'package:draftmode_formatter/formatter/date_time.dart';
+import 'package:draftmode_formatter/formatter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
